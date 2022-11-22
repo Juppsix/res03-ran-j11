0c60ad600e80c1f29b6dc435d00b15c632d82e56
Juppsix <julien.postecc@gmail.com>
2022-11-22 14:03:09 +0000

    Le message qui dÃ©crit mes changements

--- correction.js
+++ correction.js
@@ -1,26 +1,72 @@
 {
-    let emailInput = document.getElementById("#email");
-    let motdepasseInput = document.getElementById("#password");
-    let confirmemotdepasseInput = document.getElementById("#confirm-password");
-    let form1 = document.querySelector("form:first-of-type");
-    let form2 = document.querySelector("form:nth-of-type(2)");
-    let formOk = true;
+    let form = document.querySelector("form:first-of-type");
+    console.log(form);
+    let formOK=true;
     
-    form1.addEventListener("submit",function(e){
-         e.preventDefault();
-         let usernameInput = document.getElementById("#username");
-    if (usernameInput.value ()!=="")
-    {
-        e.target.submit();
-    }
-    else
-    {
-        let inputError = document.createElement("p");
-        let fieldset = document.querySelector("form: first-of-type fieldset: first-of-type");
-        inputError.innerHTML = "Veuillez choisir un nom d'utilisateur";
-        inputError.classList.add("erreur");
-        fieldset.appendChild(inputError);
-        formOk = false;
-    }
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        let usernameInput = document.querySelector("#username");
+        if (usernameInput.value  !=="")
+        {
+            e.target.submit();
+        }
+        else
+        {
+            let inputError = document.createElement("p");
+            let fieldset = document.querySelector("form:first-of-type fieldset:first-of-type");
+            
+            inputError.innerHTML ="Veuillez choisir un nom d'utilisateur";
+            inputError.classList.add("error");
+            fieldset.appendChild(inputError);
+            formOK= false;
+            usernameInput.classList.add("error");
+            
+            let EmailInput = document.querySelector("#email");
+            if (EmailInput.value !=="")
+            {
+                e.target.submit();
+            }
+            else 
+            {
+                let emailError = document.createElement("p");
+                let fieldset2 = document.querySelector("form:first-of-type fieldset:nth-of-type(2)");
+                
+                emailError.innerHTML ="Veuillez choisir un email";
+                emailError.classList.add("error");
+                fieldset2.appendChild(emailError);
+                EmailInput.classList.add("error");
+                
+                let mdpInput = document.querySelector("#password");
+                if (mdpInput.value !=="")
+                {
+                e.target.submit();
+                 }
+                else 
+                {
+            let mdpError = document.createElement("p");
+            let fieldset3 = document.querySelector("form:first-of-type fieldset:nth-of-type(3)");
+            
+            mdpError.innerHTML ="Veuillez choisir un mot de passe";
+            mdpError.classList.add("error");
+            fieldset3.appendChild(mdpError);
+            mdpInput.classList.add("error");
+            
+            
+        }
+        
+    };
+   
     
-})}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
